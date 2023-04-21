2338cbaa0ab7a0e684956b8b3b98d78fd9a6d4d2
Doug McPeek <dmcpeek@outlook.com>
2023-04-21 11:01:21 -0600

    First commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+public class BankAccount
+{
+    public BankAccount() //Constructor
+    {
+    }
+
+    private double _balance = 0;
+    public void Deposit(double amount)
+    {
+        _balance += amount;
+    }
+    public double GetBalance()
+    {
+        return _balance;
+    } 
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace BankEncapsulation
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//using C36EncapsulationDemo;
+
+using System.ComponentModel;
+using System.Security.Principal;
+
+var account = new BankAccount();
+
+account.Deposit(0);
+
+Console.WriteLine("How much would you like to deposit: ");
+var userInput = double.Parse(Console.ReadLine());
+account.Deposit(userInput);
+
+Console.WriteLine($"Your balance is $ {account.GetBalance()}");
